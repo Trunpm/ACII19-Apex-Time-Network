6ad5a282f260e1d7897e32381241c9b585bdfc3d
Trunpm <43395356+Trunpm@users.noreply.github.com>
2019-04-10 14:51:53 +0800

    Add files via upload

---
+++ ATN/HDE/TEST_CASMEII/prototxt/train_net.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+set -e
+export PYTHONPATH=$PYTHONPATH:/home/XXXX/caffe-master/python
+export PYTHONPATH=$PYTHONPATH:Data/Add_python_layers
+
+/home/XXXX/caffe-master/build/tools/caffe train --solver=ATN/HDE/Test_CASMEII/prototxt/solver.prototxt --weights=Data/facia_expression_iter_50000.caffemodel --gpu 1
